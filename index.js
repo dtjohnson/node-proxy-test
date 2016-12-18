cd094ac839f54920a1b79d402ae337530118783d
Dave Johnson <dave@dtjohnson.net>
2016-12-18 11:08:08 -0500

    Stripping hop-by-hop headers. Made keep-alive an option.

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 "use strict";
 
 const http = require('http');
-const keepAliveAgent = new http.Agent({ keepAlive: true });
+
+// From RFC 2616 section 13.5.1
+const hopByHopHeaders = [
+    "connection",
+    "keep-alive",
+    "proxy-authenticate",
+    "proxy-authorization",
+    "te",
+    "trailers",
+    "transfer-encoding",
+    "upgrade"
+];
 
 module.exports = opts => {
+    const agent = new http.Agent({ keepAlive: opts.keepAlive });
+
     return (req, res) => {
         const proxyReq = http.request({
             method: req.method,
             path: req.url,
-            headers: req.headers,
             hostname: opts.upstreamHost,
             port: opts.upstreamPort,
-            agent: keepAliveAgent
+            agent
         }, proxyRes => {
             res.statusCode = proxyRes.statusCode;
 
             for (let i = 0; i < proxyRes.rawHeaders.length; i += 2) {
                 const name = proxyRes.rawHeaders[i];
+                if (hopByHopHeaders.indexOf(name.toLowerCase()) >= 0) continue;
                 const value = proxyRes.rawHeaders[i + 1];
                 res.setHeader(name, value);
             }
 
             proxyRes.pipe(res);
         });
+
+        for (let i = 0; i < req.rawHeaders.length; i += 2) {
+            const name = req.rawHeaders[i];
+            if (hopByHopHeaders.indexOf(name.toLowerCase()) >= 0) continue;
+            const value = req.rawHeaders[i + 1];
+            proxyReq.setHeader(name, value);
+        }
 
         req.pipe(proxyReq);
 
